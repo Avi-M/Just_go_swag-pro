ebbeacb98294bbd62eb364d4892675ebf8b19f9b
Avinash Maurya <53138649+Avi-M@users.noreply.github.com>
2020-11-03 18:31:06 +0530

    Add files via upload

---
+++ SQLcon/kymain.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type test struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func main() {
+	fmt.Println("Go MySQL Tutorial")
+	serverName := "localhost:3306"
+	user := "root"
+	password := ""
+	dbName := "org"
+	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
+	// The database is called testDb
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true&multiStatements=true", user, password, serverName, dbName)
+	db, err := sql.Open("mysql", connectionString)
+	//db, err := sql.Open("mysql", "root:password @tcp(127.0.0.1:3306)/org")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// defer the close till after the main function has finished
+	// executing
+	defer db.Close()
+
+	// perform a db.Query insert
+	insert, err := db.Query("INSERT INTO test VALUES ( 4, 'Bhuniya' )")
+
+	// if there is an error inserting, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+	// be careful deferring Queries if you are using transactions
+	defer insert.Close()
+	// Execute the query
+	results, err := db.Query("SELECT id, name FROM test")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	for results.Next() {
+		var tag test
+		// for each row, scan the result into our tag composite object
+		err = results.Scan(&tag.ID, &tag.Name)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		// and then print out the tag's Name attribute
+		log.Printf(tag.Name)
+	}
+
+	var tag test
+	// Execute the query
+	err = db.QueryRow("SELECT id, name FROM test where id = ?", 2).Scan(&tag.ID, &tag.Name)
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	log.Println(tag.ID)
+	log.Println(tag.Name)
+
+}
